6744bceaabf3b774291c63cbd46fe4a91d0244c5
Gery Wahyu <gerywahyunugraha@gmail.com>
2020-05-25 12:14:13 +0800

    Add postgres and orchestrator

--- app/main.go
+++ app/main.go
@@ -1,22 +1,75 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/go-redis/redis/v7"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 	"net/http"
+	"os"
+	"url-shortener/url"
 )
 
-func main() {
-	e := echo.New()
+var useCase url.UseCase
+var cache url.CachedRepository
+var storage url.PersistentRepository
 
+func initializeCache() {
+	r := redis.NewClient(&redis.Options{
+		Addr:               os.Getenv("REDIS_ADDR"),
+		Password:           "",
+		DB:                 0,
+	})
+	cache = url.CachedRepository{Client:r}
+}
+
+func initializeStorage() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+	storage = url.PersistentRepository{DB:db}
+}
+
+func main() {
+	initializeCache()
+	initializeStorage()
+
+	useCase = url.UseCase{
+		Cache:   &cache,
+		Storage: &storage,
+	}
+
+	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", hello)
+	e.POST("/url", ShortenUrl)
 
 	e.Logger.Fatal(e.Start(":4000"))
 }
 
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+type ShortenUrlRequest struct {
+	OriginalUrl string `json:"originalUrl"`
+	ShortUrl 	string `json:"shortUrl"`
+}
+
+type ShortenUrlResponse struct {
+	ShortUrl	string `json:"shortUrl"`
+}
+
+func ShortenUrl(c echo.Context) error {
+	req := new(ShortenUrlRequest)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+	shortUrl := req.ShortUrl
+	if shortUrl == "" {
+		shortUrl = useCase.GenerateShortUrl()
+	}
+	useCase.Save(req.OriginalUrl, shortUrl)
+
+	res := ShortenUrlResponse{ShortUrl:shortUrl}
+	return c.JSON(http.StatusCreated, res)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module url-shortener
 go 1.14
 
 require (
-	github.com/go-redis/redis/v7 v7.2.0 // indirect
-	github.com/labstack/echo/v4 v4.1.16 // indirect
+	github.com/go-redis/redis/v7 v7.2.0
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/lib/pq v1.5.2
 )
